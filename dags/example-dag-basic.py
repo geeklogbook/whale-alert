fb99875c180da99438b6876ed56b1da1cd3ad418
jino.michel.aque <contact@geeklogbook.com>
2023-01-20 11:43:57 -0300

    add new functions

--- dags/example-dag-basic.py
+++ dags/example-dag-basic.py
@@ -5,28 +5,14 @@ from airflow.decorators import dag, task # DAG and task decorators for interfaci
 
 
 @dag(
-    # This defines how often your DAG will run, or the schedule by which your DAG runs. In this case, this DAG
-    # will run daily
     schedule_interval="@daily",
-    # This DAG is set to run for the first time on January 1, 2021. Best practice is to use a static
-    # start_date. Subsequent DAG runs are instantiated based on scheduler_interval
     start_date=datetime(2021, 1, 1),
-    # When catchup=False, your DAG will only run for the latest schedule_interval. In this case, this means
-    # that tasks will not be run between January 1, 2021 and 30 mins ago. When turned on, this DAG's first
-    # run will be for the next 30 mins, per the schedule_interval
     catchup=False,
     default_args={
-        "retries": 2, # If a task fails, it will retry 2 times.
+        "retries": 2,
     },
-    tags=['example']) # If set, this tag is shown in the DAG view of the Airflow UI
+    tags=['example'])
 def example_dag_basic():
-    """
-    ### Basic ETL Dag
-    This is a simple ETL data pipeline example that demonstrates the use of
-    the TaskFlow API using three simple tasks for extract, transform, and load.
-    For more information on Airflow's TaskFlow API, reference documentation here:
-    https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html
-    """
 
     @task()
     def extract():
