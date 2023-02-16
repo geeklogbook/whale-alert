ca8cf151e9776e6826666e3bbfcdb0df554f17a8
jino.michel.aque <contact@geeklogbook.com>
2023-02-15 23:31:24 -0300

    start finxing whale scrapper

--- functions/whale_alert_scrapper.py
+++ functions/whale_alert_scrapper.py
@@ -9,19 +9,25 @@ soup = BeautifulSoup(page.content, "html.parser")
 table = soup.find('table', attrs={'class': 'table'})
 table_body = table.find('tbody')
 table_rows = table_body.find_all('tr')
-crypto_dict = {"datetime_utc": [], "crypto": [], "known": [], "unknown": []}
+crypto_dict = {
+    "datetime_utc": [], 
+    "crypto": [], 
+    "known": [], 
+    "unknown": []}
 current_utc = datetime.utcnow()
 current_utc_str = current_utc.strftime("%Y%m%d")
 
 for x in table_rows:
-    coin = x.find("i").get_text().strip()
-    td_list = x.find_all("td")
-    known = td_list[1].text
-    unknown = td_list[2].text
-    crypto_dict["datetime_utc"].append(current_utc)
-    crypto_dict["crypto"].append(coin)
-    crypto_dict["known"].append(known)
-    crypto_dict["unknown"].append(unknown)
-whale_df = pd.DataFrame.from_dict(crypto_dict)
-whale_df.to_csv(f"output/whale_alert_output{current_utc_str}.csv",index=False)
-print(whale_df)
+    print(x)
+    coin = x.find("i")#get_text().strip()
+    print(coin)
+#     td_list = x.find_all("td")
+#     known = td_list[1].text
+#     unknown = td_list[2].text
+#     crypto_dict["datetime_utc"].append(current_utc)
+#     crypto_dict["crypto"].append(coin)
+#     crypto_dict["known"].append(known)
+#     crypto_dict["unknown"].append(unknown)
+# whale_df = pd.DataFrame.from_dict(crypto_dict)
+# whale_df.to_csv(f"output/whale_alert_output{current_utc_str}.csv",index=False)
+# print(whale_df)
